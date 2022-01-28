5657b91201a83e3b9fbf882a4e35fd39cb6fd666
Lucien HUA <lucien.hua@mines-paristech.fr>
2022-01-28 11:02:45 +0100

    déplace ?

--- personnage.py
+++ personnage.py
@@ -1,5 +1,6 @@
 import pygame as pg 
 game = True
+keys = pg.key.get_pressed()
 
 class Personnage:
     def __init__(self,position = [0,0],hp = 100,armor = 0,weapon = "basic knife",damage = 10):
@@ -14,6 +15,22 @@ class Personnage:
         if (self.hp < 0):
             game = False
             print("Game Over")
+
+    def attack(damage):
+        pass
+
+    def move(self):
+        if keys[pg.K_LEFT]:
+            self.position[0] -= 1
+
+        if keys[pg.K_RIGHT]:
+            self.position[0] += 1
+      
+        if keys[pg.K_UP]:
+            self.position[1] += 1
+      
+        if keys[pg.K_DOWN]:
+            self.position[1] -= 1
             
     
 class Player(Personnage): 
@@ -26,11 +43,11 @@ class Player(Personnage):
         self.inventory = inventory
         self.gold = gold
 
-    def openInventory(self):
-        #Montre l'inventaire 
-        #TODO
-    def useObject(self):
-        #TODO
+    # def openInventory(self):
+    #     #Montre l'inventaire 
+    #     #TODO
+    # def useObject(self):
+    #     #TODO
 
 
 
